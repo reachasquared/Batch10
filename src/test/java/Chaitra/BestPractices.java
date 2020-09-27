79dd5f165f22f5baaa5f532fee5f555243013412
Chaitra Pramod <sb.chaitra@gmail.com>
2020-09-27 19:59:54 -0400

    My first submission to git repository.

---
+++ src/test/java/Chaitra/BestPractices.java
@@ -0,0 +1,23 @@
+package Chaitra;
+
+
+import org.junit.Test;
+
+/**
+ * This Java file is an example about
+ * Java coding best practices
+ */
+public class BestPractices { // class begin
+    /**
+     * bestPractices method
+     */
+
+@Test
+    public void bestPractices(){ //bestPractices method begins
+
+       System.out.println("I'm in bestPractices!!");
+
+    }//bestPractices method ends
+
+}// class ends
+
---
+++ src/test/java/Chaitra/Shopping.java
@@ -0,0 +1,47 @@
+package Chaitra;
+
+import org.junit.Test;
+
+public class Shopping {// class begins
+    //1.parking lot
+    @Test
+    public void parkingLot(){//method parkinglot begins
+      System.out.println("In Parking lot");
+      mainEntrance();//calling mainEntrance method
+      toysAisle();//calling ToysAisle method
+      selectToy();//calling select toy method
+      billingCounter();//calling billing Counter method
+      parkingLot1();//calling parkingLot1 method
+
+    }//method parkinglot ends
+
+    //2.Main Entrance
+    public void mainEntrance(){ //method mainentrance begins
+        System.out.println("In Main Entrance");
+
+    }//method Main entrance ends
+
+    //3.Toys Aisle
+    public void toysAisle() {// method Toys Aisle begins
+        System.out.println("In Toys Aisle");
+
+    }//method Toys Aisle ends
+
+    //4.select toy
+    public void selectToy(){ //method Select toy begins
+        System.out.println("Select Toy");
+
+    }// method selectToy ends
+
+    //5.Billing
+    public void billingCounter(){// method BillingCounter begins
+        System.out.println("In Billing Counter");
+
+    }//Method biliingCounter ends
+
+    //6.Parking lot
+    public void parkingLot1() { // method parkingLot1 begins
+        System.out.println("In Parking Lot to Return");
+         }//method Parkinglot1 ends
+
+}//class ends
