29ded657a13eeae39898815488e90b985012e9e6
yesha patel <yesha0904@gmail.com>
2020-09-28 18:59:33 -0400

    My first submission

--- src/test/java/asquared/TestToursLogin.java
+++ src/test/java/asquared/TestToursLogin.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 public class TestToursLogin extends global {//class begins
     @Test
     public void testToursLogin(){ //method begins
+
         //Step 1	Go to http://demo.guru99.com/V1/index.php
 
+
         //Assert page title : Welcome: Mercury Tours
+
         //Verify User Name and Password fields and Sign-in button exist
 
         //Step 2	Enter asquared in user name field
