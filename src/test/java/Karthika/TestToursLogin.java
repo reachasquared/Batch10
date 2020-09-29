d7a133039565c84c5f3053bd71089ad5c8a69d13
Karthika <karthikabaljivenus@gmail>
2020-09-29 09:45:36 -0400

    My TestToursLogin submission

---
+++ src/test/java/Karthika/TestToursLogin.java
@@ -0,0 +1,40 @@
+package Karthika;
+
+import core.engine.global;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestToursLogin extends global {//class begins
+    @Test
+    public void TestToursLogin(){//method begins
+
+
+
+//Step 1	Go to http://newtours.demoaut.com
+    driver.getTitle("http://newtours.demoaut.com");
+
+//Assert page title : Welcome: Mercury Tours
+        assertEquals("Welcome: Mercury Tours",driver.getTitle());
+        driver.findElement(By.linkText("New Tours")).click();
+
+  //Verify User Name and Password fields and Sign-in button exist
+assertTrue(driver.findElement(By.name("btn sing-in")).isDisplayed());
+//Step 2	Enter asquared in user name field
+driver.findElement(By.name("asquared")).click();
+//Step 3	Enter asquared in password field
+driver.findElement(By.name("aquared")).click();
+//Step 4	Click on Sign-in
+driver.findElement(By.name("BTN sing-in")).click();
+//Assert page title is Find a Flight: Mercury Tours:
+assertEquals("Flight: Mercury Tours",driver.getTitle());
+//Step 5 Click on SIGN-OFF
+driver.findElement(By.name("BTN Sing-off")).click();
+
+
+    }//method ends
+
+
+}//class ends
